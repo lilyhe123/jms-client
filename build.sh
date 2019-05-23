a2170123c1e2c9187782e23d56b09bacf9a878e0
Lily He <lily.he@oracle.com>
2019-05-23 11:47:38 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-
+#!/bin/bash
 
 docker build -t jms-client:1.0 .
+if [ "$(kubectl get --ignore-not-found Deployment jmsclient | wc -l)" != 0 ]; then
+  kubectl delete -f jmsclient.yaml
+fi
 kubectl apply -f jmsclient.yaml
